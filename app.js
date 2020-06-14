852d6c440dd02b8c559f14a708b76fda2154fe34
tbduong <tiffaniduong@gmail.com>
2020-06-13 19:05:01 -0700

    handle 404 and all other errors, 500 error

--- app.js
+++ app.js
@@ -4,22 +4,35 @@ const app = express();
 //import Morgan middleware package (login package)
 const morgan = require('morgan');
 
+//import routes from the route folder
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
+//use morgan to help retrieve responses
 app.use(morgan('dev'));
 
-
-//import products routes
-const productRoutes = require('./api/routes/products');
+//Routes for handling requests
 app.use('/products', productRoutes);
-
-//import orders routes
-const orderRoutes = require('./api/routes/orders');
 app.use('/orders', orderRoutes);
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "Server works!"
-  });
+
+
+
+//ERROR handling
+app.use( (req, res, next) => {
+  const error = new Error('This route is NOT found!');
+  error.status = 404;
+  next(error);
 });
+
+app.use( (error, req, res, next) => {
+  res.status( error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+});
+
 
 module.exports = app;
