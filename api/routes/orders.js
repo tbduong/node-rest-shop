78e615c03a7ac80c18bc7b751a2cea24b93239f0
tbduong <tiffaniduong@gmail.com>
2020-06-13 17:27:02 -0700

    Created routing for GET and DELETE for orders and order ids

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,13 +9,29 @@ router.get( '/', (req, res, next) => {
   });
 });
 
-//POST ordersrouter.get( '/', (req, res, next) => {
+//POST orders
 router.post( '/', (req, res, next) => {
   res.status(200).json({
     message: 'Order was created'
   });
 });
 
+
+//GET individual Order by ID
+router.get('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    orderId: req.params.orderId,
+    message: "Order was successfull fetched"
+  });
+});
+
+//DELETE Individual order by ID
+router.delete('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    orderId: req.params.orderId,
+    message: "Order has been successfully deleted"
+  });
+});
 
 //The router with the routes we configured is exported and can be used in other files -- aka app.js where we import these routes
 module.exports = router;
